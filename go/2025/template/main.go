32c855a6ccef86dbc65ab546e1c2f208eff39016
erdaboss99 <60655139+erdaboss99@users.noreply.github.com>
2025-12-01 20:30:50 +0100

    add simple performance (runtime) monitoring to existing days

--- go/2025/template/main.go
+++ go/2025/template/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Part 1: ", part1("input.txt"))
-	fmt.Println("Part 2: ", part2("input.txt"))
+	start := time.Now()
+	result1 := part1("input.txt")
+	fmt.Printf("Part 1: %d (%v)\n", result1, time.Since(start))
+
+	start = time.Now()
+	result2 := part2("input.txt")
+	fmt.Printf("Part 2: %d (%v)\n", result2, time.Since(start))
 }
 
 func part1(fileName string) int {
